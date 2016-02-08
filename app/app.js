e51dec167bbebd448c00e71ad7a235e7538516f7
Sheetal Umesh Kumar <sheetaluk@gmail.com>
2016-02-08 11:33:50 -0500

    fb login

--- app/app.js
+++ app/app.js
@@ -10,4 +10,20 @@
   config(['$routeProvider', function($routeProvider) {
     $routeProvider.otherwise({redirectTo: '/entry'});
   }]);
+
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '458378737701320',
+      xfbml      : true,
+      version    : 'v2.5'
+    });
+  };
+
+  (function(d, s, id){
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) {return;}
+      js = d.createElement(s); js.id = id;
+      js.src = "//connect.facebook.net/en_US/sdk.js";
+      fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));
 }());
--- app/entry/entry.js
+++ app/entry/entry.js
@@ -10,7 +10,22 @@
     });
   }])
 
-  .controller('EntryCtrl', [function() {
+  .controller('EntryCtrl', function($scope) {
 
-  }]);
+    $scope.FBLogin = function() {
+      FB.login(function(response) {
+        if (response.authResponse) {
+          console.log('Welcome!  Fetching your information.... ');
+          FB.api('/me?fields=email,name', function(meResponse) {
+            console.log('Good to see you, ' + meResponse.name + '.');
+
+            var accessToken = FB.getAuthResponse().accessToken;
+          });
+        } else {
+          console.log('User cancelled login or did not fully authorize.');
+        }
+      }, {scope: 'public_profile,email'});
+    };
+
+  });
 }());
